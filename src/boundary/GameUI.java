486d13dacae078e471650ec8cd20210bd3fd3233
David Leong En Han <your_email@e.ntu.edu.sg>
2026-04-18 14:35:08 +0800

    fix: correct if statement in GameUI

--- src/boundary/GameUI.java
+++ src/boundary/GameUI.java
@@ -11,7 +11,7 @@ public void displayCombatantStatus(Player player, List<ICombatant> enemies) {
     System.out.printf(" Player: %-15s HP: %d/%d%n", player.getName(), player.getHp(), player.getMaxHp());
     System.out.println("----------------------------------------");
     for (ICombatant e : enemies) {
-        f (e.isAlive()) {
+        if (e.isAlive()) {
         System.out.printf("Enemy: %-16s HP: %d%n", e.getName(), e.getHp());
         }
     }
